f46042a7f016f1ae2ba7d99fdd10a04738cb747f
Bruno <brunofreitasd11@gmail.com>
2025-09-29 15:11:01 -0300

    Adiciona inicialização do projeto
    Co-Authored-By: Isabela Chaves <143914622+isabelachaves-ic@users.noreply.github.com>
    Co-Authored-By: Mauricio Bertoldo <128431515+mauricioBert@users.noreply.github.com>
    Co-Authored-By: Caio Moraes <caiobronesmor@hotmail.com>
    Co-Authored-By: Ruth Mendonça <99757116+ruthmendonca@users.noreply.github.com>

---
+++ app/src/main/java/com/example/unihub/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.unihub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unihub.ui.theme.UnihubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnihubTheme {
+                Surface (modifier = Modifier.fillMaxSize()) {
+                    Text(text="Olá!)
+              }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnihubTheme {
+        Greeting("Android")
+    }
+}
